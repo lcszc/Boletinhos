17a7e9bbbb2449525d42c0f4583a2d46af755f3c
Lucas <luke8.co@gmail.com>
2020-05-01 12:43:16 -0300

    Add initial project configuration. (#1)

---
+++ buildSrc/src/main/java/config/AppDependencies.kt
@@ -0,0 +1,10 @@
+package config
+
+object Androidx {
+    val core = "androidx.core:core-ktx:1.2.0"
+    val appCompat = "androidx.appcompat:appcompat:1.1.0"
+}
+
+object Kotlinx {
+    val stbLib = "org.jetbrains.kotlin:kotlin-stdlib:1.3.70"
+}
