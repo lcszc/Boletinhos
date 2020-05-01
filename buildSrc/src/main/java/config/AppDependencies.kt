67f47390e1e97c59b2844f6ee4ba92a391214091
Lucas Cavalcante <co.lucasc@gmail.com>
2020-05-01 14:07:17 -0300

    Add core module configuration.

--- buildSrc/src/main/java/config/AppDependencies.kt
+++ buildSrc/src/main/java/config/AppDependencies.kt
@@ -1,16 +1,25 @@
 package config
 
 object Androidx {
-    val core = "androidx.core:core-ktx:1.2.0"
-    val appCompat = "androidx.appcompat:appcompat:1.1.0"
+    const val core = "androidx.core:core-ktx:1.2.0"
+    const val appCompat = "androidx.appcompat:appcompat:1.1.0"
 
     object Room {
-        val core = "androidx.room:room-ktx:2.2.5"
-        val compiler = "androidx.room:room-compiler:2.2.5"
-        val testing = "androidx.room:room-testing:2.2.5"
+        const val core = "androidx.room:room-ktx:2.2.5"
+        const val compiler = "androidx.room:room-compiler:2.2.5"
+        const val testing = "androidx.room:room-testing:2.2.5"
     }
 }
 
 object Kotlinx {
-    val stbLib = "org.jetbrains.kotlin:kotlin-stdlib:1.3.70"
+    const val stbLib = "org.jetbrains.kotlin:kotlin-stdlib:1.3.70"
+
+    object Coroutines {
+        const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.5"
+    }
+}
+
+object Testing {
+    const val junit = "junit:junit:4.12"
+    const val assertK = "com.willowtreeapps.assertk:assertk-jvm:0.22"
 }
