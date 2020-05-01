c26ecb491e5300205056dcc91c7e96dd2eb133e4
Lucas Cavalcante <co.lucasc@gmail.com>
2020-05-01 12:53:03 -0300

    Add test logger

--- buildSrc/src/main/java/config/AppPlugins.kt
+++ buildSrc/src/main/java/config/AppPlugins.kt
@@ -3,4 +3,5 @@ package config
 object AppPlugins {
     const val androidGradle = "com.android.tools.build:gradle:4.0.0-beta05"
     const val kotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.70"
+    const val testLogger = "com.adarshr:gradle-test-logger-plugin:2.0.0"
 }
