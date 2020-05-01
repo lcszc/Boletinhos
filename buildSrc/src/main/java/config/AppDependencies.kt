214effd49bbf3ee17c285f1e48801e55457617ec
Lucas Cavalcante <co.lucasc@gmail.com>
2020-05-01 13:31:31 -0300

    Add Room dependencies.

--- buildSrc/src/main/java/config/AppDependencies.kt
+++ buildSrc/src/main/java/config/AppDependencies.kt
@@ -3,6 +3,12 @@ package config
 object Androidx {
     val core = "androidx.core:core-ktx:1.2.0"
     val appCompat = "androidx.appcompat:appcompat:1.1.0"
+
+    object Room {
+        val core = "androidx.room:room-ktx:2.2.5"
+        val compiler = "androidx.room:room-compiler:2.2.5"
+        val testing = "androidx.room:room-testing:2.2.5"
+    }
 }
 
 object Kotlinx {
