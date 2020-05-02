9887d41a458f8a6a77cad113d914b502a6727369
Corey <lucas.cavalcante@pipefy.com>
2020-05-02 13:48:28 -0300

    Add missing test case for bill status converter

---
+++ core/src/test/java/app/boletinhos/core/typeconverter/BillStatusTypeConverterTest.kt
@@ -0,0 +1,36 @@
+package app.boletinhos.core.typeconverter
+
+import app.boletinhos.core.bills.BillStatus
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.Test
+
+class BillStatusTypeConverterTest {
+    @Test fun `should convert to status from a given integer`() {
+        // @given a bill status
+        val expected = BillStatus.UNPAID
+
+        // @and its expected status code value
+        val code = BillStatus.UNPAID.code.value
+
+        // @when converting to a status from a given code
+        val actual = BillStatusTypeConverter.toStatus(code)
+
+        // @then the converted result should be `UNPAID`
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test fun `should convert to int from a given status`() {
+        // @given a bill status code value
+        val expected = BillStatus.UNPAID.code.value
+
+        // @and its bill status
+        val status = BillStatus.UNPAID
+
+        // @when converting to a integer from a given status
+        val actual = BillStatusTypeConverter.fromStatus(status)
+
+        // @then the converted result should be `0`
+        assertThat(actual).isEqualTo(expected)
+    }
+}
