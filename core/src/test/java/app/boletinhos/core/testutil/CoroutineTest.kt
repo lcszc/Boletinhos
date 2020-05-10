6bf49792925b89c0021e62347f6c370afedd1677
Lucas Cavalcante <co.lucasc@gmail.com>
2020-05-10 16:47:20 -0300

    Fix scope not throwing exception to top parent.

--- core/src/test/java/app/boletinhos/core/testutil/CoroutineTest.kt
+++ core/src/test/java/app/boletinhos/core/testutil/CoroutineTest.kt
@@ -3,6 +3,7 @@ package app.boletinhos.core.testutil
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import org.junit.Rule
@@ -11,11 +12,9 @@ abstract class CoroutineTest {
     @get:Rule
     val mainCoroutineRule = MainCoroutineRule()
 
-    val testScope = CoroutineScope(mainCoroutineRule.testDispatcher)
-
     fun <T> Flow<T>.test(block: (actual: T) -> Unit) {
-        mainCoroutineRule.runBlocking {
-            testScope.launch {
+        mainCoroutineRule.runBlocking { scope ->
+            scope.launch {
                 collect { actual ->
                     block(actual)
                 }
