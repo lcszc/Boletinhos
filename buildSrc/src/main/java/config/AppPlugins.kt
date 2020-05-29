d64b484733f7beedc735bc29e44c0277d624a3b3
Lucas Cavalcante <co.lucasc@gmail.com>
2020-05-29 11:05:20 -0300

    Bump AGP version.

--- buildSrc/src/main/java/config/AppPlugins.kt
+++ buildSrc/src/main/java/config/AppPlugins.kt
@@ -1,7 +1,7 @@
 package config
 
 object AppPlugins {
-    const val androidGradle = "com.android.tools.build:gradle:4.0.0-rc01"
+    const val androidGradle = "com.android.tools.build:gradle:4.0.0"
     const val kotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.70"
     const val testLogger = "com.adarshr:gradle-test-logger-plugin:2.0.0"
 }
