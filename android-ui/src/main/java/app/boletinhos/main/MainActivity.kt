40f5fd26366e7fbc7c46e58ec4718bf9336a06a4
Lucas Cavalcante <co.lucasc@gmail.com>
2020-06-02 23:42:20 -0300

    Update baseline theme and add launcher theme with splash behaviour.

---
+++ android-ui/src/main/java/app/boletinhos/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package app.boletinhos.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import app.boletinhos.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.App)
+        super.onCreate(savedInstanceState)
+    }
+}
