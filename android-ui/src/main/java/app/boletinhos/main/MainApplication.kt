a06532372869f00e9259dec933f024edb069879b
Lucas Cavalcante <co.lucasc@gmail.com>
2020-06-05 01:18:31 -0300

    Wire up injection components.

--- android-ui/src/main/java/app/boletinhos/main/MainApplication.kt
+++ android-ui/src/main/java/app/boletinhos/main/MainApplication.kt
@@ -1,11 +1,38 @@
 package app.boletinhos.main
 
 import android.app.Application
+import app.boletinhos.injection.context.DaggerAppContextComponent
+import app.boletinhos.injection.app.AppComponent
+import app.boletinhos.injection.app.DaggerAppComponent
+import app.boletinhos.injection.context.AppContextComponent
+import app.boletinhos.injection.preferences.DaggerPreferencesComponent
+import app.boletinhos.injection.preferences.PreferencesComponent
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 
 class MainApplication : Application() {
+    private lateinit var component: AppComponent
+
+    private fun contextComponent(): AppContextComponent {
+        return DaggerAppContextComponent.factory()
+            .create(this)
+    }
+
+    private fun preferencesComponent(): PreferencesComponent {
+        return DaggerPreferencesComponent.factory()
+            .create(contextComponent())
+    }
+
+    private fun injector(): AppComponent {
+        return DaggerAppComponent.factory()
+            .create(contextComponent(), preferencesComponent())
+            .also { component -> component.inject(this) }
+    }
+
     override fun onCreate() {
         super.onCreate()
+        component = injector()
         FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(false)
     }
+
+    fun mainComponent() = component
 }
