5083cb8a6c01589cd72db32c347433928bad105e
Lucas Cavalcante <co.lucasc@gmail.com>
2020-06-22 20:09:30 -0300

    Add DAO to create and mark bill as paid.

--- storage-bills/src/main/java/app/boletinhos/storage/database/AppDatabase.kt
+++ storage-bills/src/main/java/app/boletinhos/storage/database/AppDatabase.kt
@@ -6,6 +6,7 @@ import androidx.room.TypeConverters
 import app.boletinhos.storage.bills.BillEntity
 import app.boletinhos.storage.bills.BillsDao
 import app.boletinhos.storage.bills.BillsSummaryDao
+import app.boletinhos.storage.bills.ManageBillDao
 import app.boletinhos.storage.typeconverter.BillStatusTypeConverter
 import app.boletinhos.storage.typeconverter.LocalDateTypeConverter
 
@@ -13,6 +14,7 @@ import app.boletinhos.storage.typeconverter.LocalDateTypeConverter
 @TypeConverters(value = [LocalDateTypeConverter::class, BillStatusTypeConverter::class])
 abstract class AppDatabase : RoomDatabase() {
     abstract fun billsDao(): BillsDao
+    abstract fun manageBillDao(): ManageBillDao
     abstract fun billsSummaryDao(): BillsSummaryDao
 
     companion object {
