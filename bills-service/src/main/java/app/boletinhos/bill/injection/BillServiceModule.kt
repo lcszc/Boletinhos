07532907f97917bf6d030821a0348edde4dd39c3
Lucas Cavalcante <co.lucasc@gmail.com>
2020-06-22 23:17:47 -0300

    Update database scoping.

--- bills-service/src/main/java/app/boletinhos/bill/injection/BillServiceModule.kt
+++ bills-service/src/main/java/app/boletinhos/bill/injection/BillServiceModule.kt
@@ -2,10 +2,9 @@ package app.boletinhos.bill.injection
 
 import app.boletinhos.database.AppDatabase
 import app.boletinhos.domain.bill.BillService
-import javax.inject.Singleton
 
 @dagger.Module object BillServiceModule {
-    @dagger.Provides @Singleton internal fun provideBillService(
+    @dagger.Provides @common.AppScope internal fun provideBillService(
         database: AppDatabase
     ): BillService = database.billService()
 }
--- bills-service/src/main/java/app/boletinhos/bill/injection/ManageBillServiceModule.kt
+++ bills-service/src/main/java/app/boletinhos/bill/injection/ManageBillServiceModule.kt
@@ -2,10 +2,9 @@ package app.boletinhos.bill.injection
 
 import app.boletinhos.database.AppDatabase
 import app.boletinhos.domain.bill.ManageBillService
-import javax.inject.Singleton
 
 @dagger.Module object ManageBillServiceModule {
-    @dagger.Provides @Singleton internal fun provideManageBillService(
+    @dagger.Provides @common.AppScope internal fun provideManageBillService(
         database: AppDatabase
     ): ManageBillService = database.manageBillService()
 }
