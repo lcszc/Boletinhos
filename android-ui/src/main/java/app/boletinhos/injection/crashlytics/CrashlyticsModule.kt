db125cb063a3d0c4e97cdea4a31d8ba143fcedd6
Lucas Cavalcante <co.lucasc@gmail.com>
2020-06-22 22:56:16 -0300

    Kill CrashlyticsComponent. Make it module-only.

--- android-ui/src/main/java/app/boletinhos/injection/crashlytics/CrashlyticsModule.kt
+++ android-ui/src/main/java/app/boletinhos/injection/crashlytics/CrashlyticsModule.kt
@@ -1,12 +1,9 @@
 package app.boletinhos.injection.crashlytics
 
 import com.google.firebase.crashlytics.FirebaseCrashlytics
-import dagger.Module
-import dagger.Provides
 
-@Module
-object CrashlyticsModule {
-    @Provides fun crashlytics(): FirebaseCrashlytics {
+@dagger.Module object CrashlyticsModule {
+    @dagger.Provides @common.AppScope internal fun provideCrashlytics(): FirebaseCrashlytics {
         return FirebaseCrashlytics.getInstance()
     }
 }
