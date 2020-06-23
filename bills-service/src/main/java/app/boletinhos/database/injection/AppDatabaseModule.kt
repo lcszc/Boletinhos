18a3cab71d335c42110fbcd1c2e9a99726cc6683
Lucas Cavalcante <co.lucasc@gmail.com>
2020-06-23 19:08:36 -0300

    Wrap annotations line to ease lecture.

--- bills-service/src/main/java/app/boletinhos/database/injection/AppDatabaseModule.kt
+++ bills-service/src/main/java/app/boletinhos/database/injection/AppDatabaseModule.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import androidx.room.Room
 import app.boletinhos.database.AppDatabase
 
-@dagger.Module object AppDatabaseModule {
-    @dagger.Provides @common.AppScope internal fun provideAppDatabase(
+@dagger.Module
+object AppDatabaseModule {
+    @dagger.Provides
+    @common.AppScope
+    internal fun provideAppDatabase(
         @common.AppContext context: Context
     ): AppDatabase {
         return Room
