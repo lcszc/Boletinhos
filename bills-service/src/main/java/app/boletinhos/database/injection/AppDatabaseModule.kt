4cffb874cd217c5056eedceb20dda48615b399fb
Lucas Cavalcante <co.lucasc@gmail.com>
2020-06-22 23:17:47 -0300

    Update database scoping.

--- bills-service/src/main/java/app/boletinhos/database/injection/AppDatabaseModule.kt
+++ bills-service/src/main/java/app/boletinhos/database/injection/AppDatabaseModule.kt
@@ -3,10 +3,9 @@ package app.boletinhos.database.injection
 import android.content.Context
 import androidx.room.Room
 import app.boletinhos.database.AppDatabase
-import javax.inject.Singleton
 
 @dagger.Module object AppDatabaseModule {
-    @dagger.Provides @Singleton internal fun provideAppDatabase(
+    @dagger.Provides @common.AppScope internal fun provideAppDatabase(
         @common.AppContext context: Context
     ): AppDatabase {
         return Room
--- bills-service/src/main/java/app/boletinhos/summary/injection/SummaryServiceModule.kt
+++ bills-service/src/main/java/app/boletinhos/summary/injection/SummaryServiceModule.kt
@@ -2,10 +2,9 @@ package app.boletinhos.summary.injection
 
 import app.boletinhos.database.AppDatabase
 import app.boletinhos.domain.summary.SummaryService
-import javax.inject.Singleton
 
 @dagger.Module object SummaryServiceModule {
-    @dagger.Provides @Singleton internal fun provideSummaryService(
+    @dagger.Provides @common.AppScope internal fun provideSummaryService(
         database: AppDatabase
     ): SummaryService = database.summaryService()
 }
