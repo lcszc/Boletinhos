927b61c0efa8e9aeb8ff0e7127ed3fb3164bf922
Lucas Cavalcante <co.lucasc@gmail.com>
2020-06-22 23:10:40 -0300

    Introduce AppContext.

--- bills-service/src/main/java/app/boletinhos/di/AppDatabaseModule.kt
+++ bills-service/src/main/java/app/boletinhos/di/AppDatabaseModule.kt
@@ -6,7 +6,9 @@ import app.boletinhos.database.AppDatabase
 import javax.inject.Singleton
 
 @dagger.Module object AppDatabaseModule {
-    @dagger.Provides @Singleton internal fun provideAppDatabase(context: Context): AppDatabase {
+    @dagger.Provides @Singleton internal fun provideAppDatabase(
+        @common.AppContext context: Context
+    ): AppDatabase {
         return Room
             .databaseBuilder(context, AppDatabase::class.java, AppDatabase.DATABASE_NAME)
             .build()
