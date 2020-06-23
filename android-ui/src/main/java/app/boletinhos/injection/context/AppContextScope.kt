abbe765ea7d4cccbea9f4dcffaceb4518fe30887
Lucas Cavalcante <co.lucasc@gmail.com>
2020-06-22 22:59:13 -0300

    Kill ContextComponent.

--- android-ui/src/main/java/app/boletinhos/injection/context/AppContextScope.kt
+++
@@ -1,5 +0,0 @@
-package app.boletinhos.injection.context
-
-import javax.inject.Scope
-
-@Scope @Retention annotation class AppContextScope
--- android-ui/src/main/java/app/boletinhos/main/MainApplication.kt
+++ android-ui/src/main/java/app/boletinhos/main/MainApplication.kt
@@ -4,21 +4,14 @@ import android.app.Application
 import app.boletinhos.crashcat.CrashCat
 import app.boletinhos.injection.app.AppComponent
 import app.boletinhos.injection.app.DaggerAppComponent
-import app.boletinhos.injection.context.AppContextComponent
-import app.boletinhos.injection.context.DaggerAppContextComponent
 import javax.inject.Inject
 
 class MainApplication : Application() {
     private lateinit var component: AppComponent
 
-    private fun contextComponent(): AppContextComponent {
-        return DaggerAppContextComponent.factory()
-            .create(this)
-    }
-
     private fun injector(): AppComponent {
         return DaggerAppComponent.factory()
-            .create(contextComponent())
+            .create(this)
             .also { component -> component.inject(this) }
     }
 
