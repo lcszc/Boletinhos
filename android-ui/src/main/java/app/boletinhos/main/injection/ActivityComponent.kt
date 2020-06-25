65639f2dc0627563a0320f7133761b944d2aea37
Corey <co.lucasc@gmail.com>
2020-06-25 01:54:29 -0300

    Add LifecycleCoroutineScope injection in activity component.

--- android-ui/src/main/java/app/boletinhos/main/injection/ActivityComponent.kt
+++ android-ui/src/main/java/app/boletinhos/main/injection/ActivityComponent.kt
@@ -1,8 +1,9 @@
 package app.boletinhos.main.injection
 
 import android.app.Activity
+import app.boletinhos.lifecycle.injection.LifecycleCoroutineScopeModule
 
-@dagger.Subcomponent
+@dagger.Subcomponent(modules = [LifecycleCoroutineScopeModule::class])
 interface ActivityComponent {
     @dagger.Subcomponent.Factory interface Factory {
         fun create(
