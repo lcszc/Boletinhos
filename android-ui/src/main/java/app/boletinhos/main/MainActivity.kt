37d20426461d25653dba48d31f3d8536e373c163
Corey <co.lucasc@gmail.com>
2020-06-25 02:03:40 -0300

    Set up activity component.

--- android-ui/src/main/java/app/boletinhos/main/MainActivity.kt
+++ android-ui/src/main/java/app/boletinhos/main/MainActivity.kt
@@ -3,8 +3,21 @@ package app.boletinhos.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import app.boletinhos.R
+import app.boletinhos.application.MainApplication
+import app.boletinhos.application.injection.AppComponent
+import app.boletinhos.main.injection.ActivityComponent
 
 class MainActivity : AppCompatActivity() {
+    private fun appComponent(): AppComponent {
+        return (application as MainApplication).mainComponent()
+    }
+
+    private fun injection(): ActivityComponent {
+        return appComponent()
+            .activityComponentFactory()
+            .create(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.App)
         super.onCreate(savedInstanceState)
