bb3fb06f511ba2fb51e5da425f906c8ac24168c3
Corey <co.lucasc@gmail.com>
2020-06-25 21:59:58 -0300

    Rework LifecycleCoroutineScope.

--- android-core/src/main/java/app/boletinhos/lifecycle/LifecycleCoroutineScope.kt
+++ android-core/src/main/java/app/boletinhos/lifecycle/LifecycleCoroutineScope.kt
@@ -1,15 +1,16 @@
 package app.boletinhos.lifecycle
 
 import com.zhuinden.simplestack.ScopedServices
+import common.CoroutineScopeContainer
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
+import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
-internal class LifecycleCoroutineScope(
-    dispatcher: CoroutineDispatcher
-): CoroutineScope, ScopedServices.Registered {
+internal class LifecycleCoroutineScope @Inject constructor(
+    @common.UiDispatcher dispatcher: CoroutineDispatcher
+): CoroutineScopeContainer, ScopedServices.Registered {
     override val coroutineContext: CoroutineContext = SupervisorJob() + dispatcher
 
     override fun onServiceRegistered() = Unit
