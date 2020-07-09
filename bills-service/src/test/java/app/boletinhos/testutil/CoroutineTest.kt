fb5169c55e388df5936c62e23db1bf03092cbc72
Lucas Cavalcante <co.lucasc@gmail.com>
2020-07-09 00:24:01 -0300

    Add testutil module.

--- bills-service/src/test/java/app/boletinhos/testutil/CoroutineTest.kt
+++ bills-service/src/test/java/app/boletinhos/testutil/CoroutineTest.kt
@@ -5,10 +5,11 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import org.junit.Rule
+import testutil.MainCoroutineRule
+import testutil.runBlocking
 
 abstract class CoroutineTest {
-    @get:Rule
-    val mainCoroutineRule = MainCoroutineRule()
+    @get:Rule val mainCoroutineRule = MainCoroutineRule()
 
     fun <T> Flow<T>.test(block: (actual: T) -> Unit) {
         mainCoroutineRule.runBlocking { scope ->
