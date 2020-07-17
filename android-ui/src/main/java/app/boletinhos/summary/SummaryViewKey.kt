5071a95464732ab6a1230dd17e91faae4c7b52e6
Lucas Cavalcante <co.lucasc@gmail.com>
2020-07-17 00:01:18 -0300

    Setting up injection.

--- android-ui/src/main/java/app/boletinhos/summary/SummaryViewKey.kt
+++ android-ui/src/main/java/app/boletinhos/summary/SummaryViewKey.kt
@@ -5,6 +5,7 @@ import app.boletinhos.main.injection.activityRetainedComponent
 import app.boletinhos.navigation.ViewKey
 import com.zhuinden.simplestack.ServiceBinder
 import com.zhuinden.simplestackextensions.services.DefaultServiceProvider
+import com.zhuinden.simplestackextensions.servicesktx.add
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
@@ -15,5 +16,6 @@ class SummaryViewKey : ViewKey, DefaultServiceProvider.HasServices {
 
     override fun bindServices(serviceBinder: ServiceBinder) {
         val activityRetainedComponent = serviceBinder.activityRetainedComponent
+        serviceBinder.add(activityRetainedComponent.summaryViewModel())
     }
 }
