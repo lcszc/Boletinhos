f5b67e307a23d38ff625dc879286717f557bbe8f
Corey <co.lucasc@gmail.com>
2021-02-15 21:17:13 -0300

    Add TextInput component.

---
+++ android-core/src/main/java/app/boletinhos/widget/text/TextInput.kt
@@ -0,0 +1,27 @@
+package app.boletinhos.widget.text
+
+import android.content.Context
+import android.util.AttributeSet
+import app.boletinhos.core.databinding.TextInputContentBinding
+import app.boletinhos.ext.view.inflater
+import com.google.android.material.textfield.TextInputLayout
+
+open class TextInput(
+    context: Context,
+    attrs: AttributeSet? = null
+) : TextInputLayout(context, attrs) {
+    private lateinit var inputBinding: TextInputContentBinding
+
+    open var value: String
+        get() = inputBinding.input.text?.toString().orEmpty()
+        set(value) = setTextInternal(value)
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        inputBinding = TextInputContentBinding.inflate(inflater, this, true)
+    }
+
+    private fun setTextInternal(value: String) {
+        inputBinding.input.setText(value)
+    }
+}
