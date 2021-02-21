5d6490cf42354cfc0fa744f32f3e1100030b6f32
Corey <co.lucasc@gmail.com>
2021-02-21 00:52:53 -0300

    Add matcher to assert DateInput date value.

---
+++ android-ui/src/androidTest/java/app/boletinhos/testutil/DateInputMatcher.kt
@@ -0,0 +1,20 @@
+package app.boletinhos.testutil
+
+import android.view.View
+import androidx.test.espresso.matcher.BoundedMatcher
+import app.boletinhos.widget.date.DateInput
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import java.time.LocalDate
+
+fun dateInputHasDate(date: LocalDate) : Matcher<View?> {
+    return object : BoundedMatcher<View?, DateInput>(DateInput::class.java) {
+        override fun matchesSafely(item: DateInput): Boolean {
+            return date == item.date
+        }
+
+        override fun describeTo(description: Description?) {
+            description?.appendText("date is $date in DateInput")
+        }
+    }
+}
