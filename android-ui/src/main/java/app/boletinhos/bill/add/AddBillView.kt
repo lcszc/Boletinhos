fb7ac72cef103bc55779841304f98abc549725ea
Corey <co.lucasc@gmail.com>
2021-02-21 23:20:25 -0300

    Introduce add bill content into a new layout.

---
+++ android-ui/src/main/java/app/boletinhos/bill/add/AddBillView.kt
@@ -0,0 +1,9 @@
+package app.boletinhos.bill.add
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+class AddBillView(context: Context, attrs: AttributeSet? = null) : RelativeLayout(context, attrs) {
+
+}
